41f9fabf98d2770dd4a4819d3242ea0d3c688d17
Saleh Aly <saleh@novelari.com>
2018-12-15 16:13:17 +0200

    Setup database and enable pgcrypto to use uuid instead of id

--- config/application.rb
+++ config/application.rb
@@ -17,10 +17,15 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module LibraryManagementApiRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.generators do |g|
+      g.orm :active_record, primary_key_type: :uuid
+      g.orm :active_record, foreign_key_type: :uuid
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
